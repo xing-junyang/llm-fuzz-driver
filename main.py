61b836b081317be0316ad26f426bfa295812d248
221250153-Wang Jing <221250153@smail.nju.edu.cn>
2025-01-06 01:23:33 +0800

    fixed: fixed minor bugs

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ if __name__ == "__main__":
         if result == "Valid Driver":
             print("Driver generated successfully.")
             # move the generated driver to the valid drivers directory '/outputs/validated_fuzz_drivers'
+            os.makedirs(current_file_path + "/outputs/validated_fuzz_drivers", exist_ok=True)
             with open(current_file_path + "/outputs/validated_fuzz_drivers/valid_driver.c", "w") as file:
                 file.write(driver_code)
             state = "success"
